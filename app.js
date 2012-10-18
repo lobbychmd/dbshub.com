540f14018e45c4f340edb05957caba1a018415cd
lobbychmd <lobbychmd@gmail.com>
2012-10-18 18:22:55 +0800

    3

---
+++ app.js
@@ -0,0 +1,41 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes');
+
+var app = module.exports = express.createServer();
+var db = require('mongo');
+db.connect();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jqtpl');
+  app.register(".html", require("jqtpl").express);
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+// Routes
+
+app.get('/', routes.index);
+app.get('/table', routes.table);
+app.get('/data/table', routes.metaTable);
+
+app.listen(3000, function(){
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
---
+++ routes/index.js
@@ -0,0 +1,41 @@
+//var metaTable = require("metaTable").metaTable;
+var mongoose = require('mongoose');
+var Enumerable = require('linq');
+
+var metaTable = function (doc) {
+    this.TableName = doc.TableName;
+
+    for (var i in doc.Indexies)
+        if (doc.Indexies[i].PrimaryKey) {
+            //var
+        }
+    this.cols = doc.Columns.slice(10);
+    Enumerable.From(doc.cols).ForEach(function (obj) {
+        obj.PK = true;
+        //console.log(obj.PK);
+    });
+
+    this.Get = function () {
+        return { TableName: this.TableName };
+    }
+}
+
+exports.index = function (req, res) {
+    res.render('index.html', {});
+};
+
+exports.metaTable = function (req, res) {
+    var db = require('mongo');
+    var tables = mongoose.model("MetaTable").findOne(function (err, doc) {
+        res.send(new metaTable(doc));
+    });
+};
+
+exports.table = function (req, res) {
+    var db = require('mongo');
+    var tables = mongoose.model("MetaTable").findOne(function (err, doc) {
+        var data1 = new metaTable(doc);
+        data1.layout = null;
+        res.render("table.html", data1);
+    });
+};
