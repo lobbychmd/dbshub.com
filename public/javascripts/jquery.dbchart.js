d77726e4450946b92e095c9124472cf75ab101f0
lobbychmd <lobbychmd@gmail.com>
2012-11-05 18:22:32 +0800

    1

--- public/javascripts/jquery.dbchart.js
+++ public/javascripts/jquery.dbchart.js
@@ -1,10 +1,56 @@
 ﻿$.fn.dbchart = function () {
     return this.each(function () {
-        //$.dialog.init();
-        //$(this).find('a.edit').click(function () {
-        //    alert($(this).closest('.flowNode').html());
-        //    return false;
-        //});
+        $('a.addIndex').live('click', function () {
+            var table = $(this).closest('.tb-design');
+            $('#tpIndex').tmpl({}).insertBefore($(this).closest('th')).addClass('index');
+            $('#tpIndexColumn').tmpl({}).insertAfter(table.find('tbody td:last-child'));
+            return false;
+        });
+        $('a.index').live('click', function () {
+            var th = $(this).closest('th');
+            var table = $(this).closest('.tb-design');
+            var indexType = th.attr('indexType');
+            if (!indexType) indexType = 'I';
+            if (indexType == 'I') indexType = 'U';
+            else if (indexType == 'U') {
+                if (table.find('th[indexType=P]').size() > 0) indexType = 'I';
+                else indexType = 'P';
+            }
+            else indexType = 'I';
+            th.attr('indexType', indexType);
+            return false;
+        });
+        $('td.index').live('click', function () {
+            var idx = $(this).children('span');
+            var table = $(this).closest('.tb-design');
+            if (idx.size() == 0) {
+                var max = table.find('tbody td.index:nth-child(' + (this.cellIndex + 1) + ') span').size() + 1;
+                $('<span>').text(max).appendTo(this);
+            }
+            else {
+                table.find('tbody td.index:nth-child(' + (this.cellIndex + 1) + ') span').each(function () {
+                    if (parseInt($(this).text()) > parseInt(idx.text())) $(this).text(parseInt($(this).text()) - 1);
+                });
+                idx.remove();
+            }
+        });
+        $('a.delIndex').live('click', function () {
+            var idx = $(this).closest('th')[0].cellIndex;
+            var table = $(this).closest('.tb-design');
+            if (confirm('要删除索引吗?')) {
+                table.find('thead th.index:nth-child(' + (idx + 1) + ')').remove();
+                table.find('tbody td.index:nth-child(' + (idx + 1) + ')').remove();
+            }
+        });
+        $('a.rename').live('click', function () {
+            var th = $(this).closest('th');
+            if (th.hasClass('editing')) th.removeClass('editing');
+            else th.addClass('editing').find('input').focus();
+        });
+        $('input').live('blur', function () {
+            var th = $(this).closest('th');
+            th.removeClass('editing');
+        });
     });
 }
 
