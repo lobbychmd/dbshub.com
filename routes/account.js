5c4d437c6c012c973613b5356d49f2af69176422
lobbychmd <lobbychmd@gmail.com>
2012-11-19 22:46:27 +0800

    123

--- routes/account.js
+++ routes/account.js
@@ -7,9 +7,9 @@ exports.projects = function (callback) {
     auth.demo_data();
 
     var query_prjs = {};
-    auth.limit(query_prjs, 'dev'); //限制只有开发者才可以浏览
+    query_prjs = auth.limit(query_prjs, 'dev'); //限制只有开发者才可以浏览
+    console.log(query_prjs);
     config.db().collection('Project').find(query_prjs).toArray(function (err, items) {
-        
         callback(items);
     });
 }
--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,10 @@ var account = require('./account');
 exports.index = function (req, res) {
     //var auth = snsauth.snsauth();
     account.projects(function(items){
-       // console.dir(items);
+        console.dir(items);
     });
     res.render('index.html', { toolbar: null });
 };
-
- 
 
 exports.tables = function (req, res) {
     var m = mongoose.model("MetaTable");
